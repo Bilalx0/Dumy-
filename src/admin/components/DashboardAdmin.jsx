3f83379178ba4657d663f474bc8761f98eee178c
Asad_noob69 <asadaliabbasi787@gmail.com>
2025-04-07 21:08:15 +0500

    updates

--- src/admin/components/DashboardAdmin.jsx
+++ src/admin/components/DashboardAdmin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import Sidebar from "../components/Sidebar";
-import Dashboard from "../components/Dashboard";
+import Sidebar from "../../components/Sidebar";
+import Dashboard from "./Dashboard";
 
 const DashboardAdmin = () => {
   const [viewType, setViewType] = useState("mansions"); // Default to Mansions

3a6340d06346e438b9945a2ed44117515e2f51d6
Asad_noob69 <asadaliabbasi787@gmail.com>
2025-04-07 21:10:14 +0500

    new side

--- src/admin/DashboardAdmin.jsx
+++ src/admin/DashboardAdmin.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Sidebar from "../components/Sidebar";
+import Sidebar from "./components/Sidebar";
 import Dashboard from "./components/Dashboard";
 
 const DashboardAdmin = () => {
